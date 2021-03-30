7e9802718ae790b38c61e29e9a751421c1cb53dc
Ryogassa Avatara <80819260+RyogassaAvatara@users.noreply.github.com>
2021-03-30 10:02:53 +0700

    Add files via upload

---
+++ PrimeNumberChecker/primeNumbersChecker.java
@@ -0,0 +1,37 @@
+public class primeNumbersChecker {
+
+    public static void main(String[] args) {
+
+        int i;
+        int num = 0;
+        int maxCheck = 500; 
+        boolean isPrime = true;
+
+        //Empty String
+        String primeNumbersFound = "";
+
+        //Start loop 1 to maxCheck
+        for (i = 1; i <= maxCheck; i++) {
+            isPrime = CheckPrime(i);
+            if (isPrime) {
+                primeNumbersFound = primeNumbersFound + i + " ";
+            }
+        }
+        System.out.println("Prime numbers from 1 to " + maxCheck + " are:");
+        // Print prime numbers from 1 to maxCheck
+        System.out.println(primeNumbersFound);
+    }
+    public static boolean CheckPrime(int numberToCheck) {
+        int remainder;
+        for (int i = 2; i <= numberToCheck / 2; i++) {
+            remainder = numberToCheck % i;
+            
+            if (remainder == 0) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+
+}
